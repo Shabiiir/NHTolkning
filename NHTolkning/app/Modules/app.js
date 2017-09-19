5980d897031598360dc4ee450d5e800edcd0c877
Shabir Rezai <shabir.rezai@gmail.com>
2017-09-19 22:16:18 +0200

    Adding files.

---
+++ NHTolkning/app/Modules/app.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module("myApp", ["ngRoute"]);
+    app.config(function ($routeProvider) {
+        $routeProvider
+        .when("/", {
+            templateUrl: 'home/home.html'
+        })
+        .when("/home", {
+            templateUrl: "home/home.html"
+        })
+        .when("/about", {
+            templateUrl: "about/about.html"
+        })
+        .when("/contact", {
+            templateUrl: "contact/contact.html"
+        })
+        .otherwise({ redirectTo: '/' });
+    });
+
